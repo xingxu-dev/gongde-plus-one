7eb11982f19bae3e73a77a31c94b10048d0d857d
xuxing <xuxing@baijia.com>
2025-05-14 18:51:34 +0800

    Initial commit: 功德+1 小程序

---
+++ App.vue
@@ -0,0 +1,38 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	page {
+		background-color: #F5E6D3;
+		font-family: "楷体", KaiTi, serif;
+	}
+	
+	/* 去除按钮默认边框 */
+	button {
+		border: none;
+		padding: 0;
+		margin: 0;
+		background: none;
+	}
+	
+	button::after {
+		border: none;
+	}
+	
+	/* 去除图片默认间距 */
+	image {
+		vertical-align: middle;
+	}
+</style>
---
+++ pages/index/index.js
@@ -0,0 +1,38 @@
+Page({
+  data: {
+    count: 0,
+    isActive: false
+  },
+
+  onLoad() {
+    // 创建音频上下文
+    this.audioContext = wx.createInnerAudioContext();
+    this.audioContext.src = '/static/wooden-fish-sound.wav';
+  },
+
+  tapWoodenFish() {
+    // 播放音效
+    this.audioContext.stop();
+    this.audioContext.play();
+    
+    // 增加计数
+    this.setData({
+      count: this.data.count + 1,
+      isActive: true
+    });
+
+    // 重置动画状态
+    setTimeout(() => {
+      this.setData({
+        isActive: false
+      });
+    }, 100);
+  },
+
+  onUnload() {
+    // 页面卸载时释放音频资源
+    if (this.audioContext) {
+      this.audioContext.destroy();
+    }
+  }
+}) 
